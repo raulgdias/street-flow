7caa7c74b274086642f90d679e1d9585dc7f6841
Raul Dias <raulgdias97@gmail.com>
2026-08-07 19:03:12 -0300

    fix: adiciona types de multer e tipagem NestExpressApplication no main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { existsSync, mkdirSync } from 'fs';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const uploadPath = join(__dirname, '..', 'uploads');
   if (!existsSync(uploadPath)) {
